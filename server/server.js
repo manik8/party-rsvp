0d8a88bb5959b9f2ed4986d0f62fd464d4e4837f
manik8 <maniksharma2014.ms@gmail.com>
2020-08-21 21:39:12 +0530

    Party Invitation

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 8888;
 
 connectDB();
 
-app.use(express.json({ extended: true}));
+app.use(express.json({ extended: true }));
 
-app.use('/register', require('./routes/register'));
+app.use("/register", require("./routes/register"));
 
-app.use('/auth', require('./routes/auth'));
+app.use("/auth", require("./routes/auth"));
 
-app.use('/guests', require('./routes/guests'));
+app.use("/guests", require("./routes/guests"));
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_env === "production") {
+  app.use(express.static("../client/build"));
+}
 app.listen(PORT, () => console.log(`Server is Started at PORT ${PORT}`));
